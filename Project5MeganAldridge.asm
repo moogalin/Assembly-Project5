d932fad84dd91788ca321f401a1a467c5e1ce96f
Megan Aldridge <m.aldridge@live.com>
2016-02-22 15:06:13 -0600

    Added files via upload

--- Project5MeganAldridge.asm
+++ Project5MeganAldridge.asm
@@ -12,53 +12,113 @@ TITLE Program 5    (program5MeganAldridge.asm)
 INCLUDE Irvine32.inc
 
 ; constants
-lowerLimitUser = 10
-upperLimitUser = 200
+user_MIN = 10
+user_MAX = 200
 
-lowerLimitRand = 100
-upperLimitRand = 999
+rand_LO = 100
+rand_HIGH = 999
 
 .data
 
-introString				BYTE	"Sorting Random Integers         Programmed by Megan Aldridge" 
+introString				BYTE	"Sorting Random Integers         Programmed by Megan Aldridge", 0dh, 0ah 
 						BYTE	"This program generates random numbers in the range [100 ... 999], ", 0dh, 0ah
 						BYTE	"displays the original list, sorts the list, and calculates the, ", 0dh, 0ah
-						BYTE	"median value. Finally, it displays the list sorted in descending order. " , 0
+						BYTE	"median value. Finally, it displays the list sorted in descending order. " , 0dh, 0ah, 0
 						;BYTE	"**EC: Program numbers the line of each user input", 0dh, 0ah, 0
-userInstructString		BYTE	"How many numbers should be generated? [10 ... 200]: ", 0dh, 0ah, 0
+userInstructString		BYTE	"How many numbers should be generated? [10 ... 200]: ", 0
 invalidInputString		BYTE	"Invalid input", 0dh, 0ah, 0
 unsortedString			BYTE	"The unsorted random numbers:", 0dh, 0ah, 0
 medianString			BYTE	"The median is ", 0
 sortedString			BYTE	"The sorted random numbers:", 0dh, 0ah, 0
+arrayRand				DWORD	200 DUP(?)
+;arrayCount				DWORD	0
+userRequest				DWORD	?
+randInt					DWORD	?
+endString				BYTE	"Made it back", 0dh, 0ah, 0
+
 
 
 .code
 main PROC
-	
-	call intro
-	call getData
-	call fillArray
-	call sortList
-	call displayList		; before sorting
-	call displayMedian
-	call displayList		; after sorting 
+		call	Randomize
 
-	exit	; exit to operating system
+		push	OFFSET	introString				; pass introString by reference
+		call	intro
+
+		push	OFFSET	invalidInputString
+		push	OFFSET	userRequest				; 
+		push	OFFSET	userInstructString	
+		call	getData
+
+		mov		eax, userRequest
+		call	WriteDec
+		call	fillArray
+		call	sortList
+		call	displayList				; before sorting
+		call	displayMedian
+		call	displayList				; after sorting 
+
+		mov		edx, OFFSET endString
+		call	WriteString
+		exit							; exit to operating system
 main ENDP
 
+;Procedure to display introductory statement to user. 
+;receives:			OFFSET of intro string
+;returns:			none
+;preconditions:		none
+;registers changed: none
+
 intro PROC
-ret
+		push	ebp
+		mov		ebp, esp
+		pushad
+
+		mov		edx, [ebp+8]
+		call	WriteString
+		call	Crlf
+
+		popad
+		mov		esp, ebp
+		pop		ebp
+		ret		4
 intro ENDP
 
 
-;Procedure to get values for userData. This Procedure calls another 
-;	procedure to perform validation. 
-;receives:			none
-;returns:			none
+;Procedure to receive and validate userRequest integer. 
+;receives:			OFFSET for the following strings: invalid userInstruct and invalidInput.
+;					OFFSET for integer data userRequest
+;returns:			user input in global userRequest 
 ;preconditions:		none
-;registers changed: edx, eax 
+;registers changed: none
 getData PROC
-ret
+		push	ebp
+		mov		ebp, esp
+		pushad
+
+	beginGetData:
+		mov		edx, [ebp+8]
+		call	WriteString
+		call	ReadInt
+		cmp		eax, user_MIN
+		jl		invalidMessage
+		cmp		eax, user_MAX
+		jg		invalidMessage
+		jmp		endGetData
+
+	invalidMessage:
+		mov		edx, [ebp+16]
+		call	WriteString
+		jmp		beginGetData
+
+	endGetData:
+		mov		ebx, [ebp+12]
+		mov		[ebx], eax
+
+		popad
+		mov		esp, ebp
+		pop		ebp
+		ret		12
 getData ENDP
 
 
